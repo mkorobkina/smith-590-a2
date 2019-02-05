f559c8c1932c59ad16d999dbb3d4b4975f3fc7ce
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2019-02-05 15:08:41 -0500

    Formatted

--- HostCountMapperQ1.java
+++ HostCountMapperQ1.java
@@ -1,3 +1,4 @@
+
 // map function for application to count the number of
 // times each unique IP address 4-tuple appears in an
 // adudump file.
@@ -8,27 +9,26 @@ import java.net.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.util.*;
 import org.apache.hadoop.mapreduce.Mapper;
-public class HostCountMapper
-extends Mapper<LongWritable, Text, Text, IntWritable> {
-@Override
-public void map(LongWritable key, Text value, Context context)
-throws IOException, InterruptedException {
-String line = value.toString();
-String[] tokens = line.split("\\s");
-String IPaddr1 = new String();
-String IPaddr2 = new String();
-int last_dot;
-// get the two IP address.port fields
-IPaddr1 = tokens[2];
-IPaddr2 = tokens[4];
-// eliminate the port part
-last_dot = IPaddr1.lastIndexOf('.');
-IPaddr1 = IPaddr1.substring(0, last_dot);
-last_dot = IPaddr2.lastIndexOf('.');
-IPaddr2 = IPaddr2.substring(0, last_dot);
-// output the key, value pairs where the key is an
-// IP address 4-tuple and the value is 1 (count)
-context.write(new Text(IPaddr1), new IntWritable(1));
-context.write(new Text(IPaddr2), new IntWritable(1));
-}
+
+public class HostCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] tokens = line.split("\\s");
+        String IPaddr1 = new String();
+        String IPaddr2 = new String();
+        int last_dot;
+        // get the two IP address.port fields
+        IPaddr1 = tokens[2];
+        IPaddr2 = tokens[4];
+        // eliminate the port part
+        last_dot = IPaddr1.lastIndexOf('.');
+        IPaddr1 = IPaddr1.substring(0, last_dot);
+        last_dot = IPaddr2.lastIndexOf('.');
+        IPaddr2 = IPaddr2.substring(0, last_dot);
+        // output the key, value pairs where the key is an
+        // IP address 4-tuple and the value is 1 (count)
+        context.write(new Text(IPaddr1), new IntWritable(1));
+        context.write(new Text(IPaddr2), new IntWritable(1));
+    }
 }
