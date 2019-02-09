96963b0dcd375546e82105ca231f7a83c5c5e1d0
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2019-02-09 12:36:31 -0500

    Update HostCountReducerQ2.java

--- HostCountReducerQ2.java
+++ HostCountReducerQ2.java
@@ -10,18 +10,22 @@ public class HostCountReducer extends Reducer<Text, Text, LongWritable, LongWrit
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
-        long max;
-        long min;
+        long max = -1;
+        long min = -1;
         long totalBytes = 0;
         int i = 0;
-        for (Text value : values) { //Is this iteration correct? Not sure
-            String val = value.getValue();
-            convertedVal = Long.parseLong(val);
+        
+        String strKey = key.toString();
+        long bytes = Long.parseLong(strKey);
+        
+        for (Text value : values) {
+            String val = value.toString();
+            long convertedVal = Long.parseLong(val);
             
             if (i == 0) {
-             max = convertedVal; //To start off, set the first value as the max
-             min = convertedVal; //Set the first value as the min as well
-             i++;
+                max = convertedVal; //To start off, set the first value as the max
+                min = convertedVal; //Set the first value as the min as well
+                i++;
             }
             
             if (convertedVal > max) {
@@ -33,14 +37,14 @@ public class HostCountReducer extends Reducer<Text, Text, LongWritable, LongWrit
             }
             
         }
-        
-        for (Text key : keys) { //Is this iteration correct?
-            String stringKey = key.getValue();
-            long bytes = Long.parseLong(stringKey);
-            totalBytes += bytes; //Sum all the bytes
-        }
-        
+                   
+//         for (Text key : keys) { //Is this iteration correct? No -- I think this can be done more easily in the command line?
+//             String stringKey = key.toString();
+//             long bytes = Long.parseLong(stringKey);
+//             totalBytes += bytes; //Sum all the bytes
+//         }
+
         long elapsedTime = max - min;
-        context.write(new LongWritable(totalBytes), new LongWritable(elapsedTime));
+        context.write(new LongWritable(bytes), new LongWritable(elapsedTime));
     }
 }
