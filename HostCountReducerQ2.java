2767c9d5e114713d9470bf7e4a40a79574a0bb0b
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2019-02-13 07:27:09 -0500

    Update HostCountReducerQ2.java

--- HostCountReducerQ2.java
+++ HostCountReducerQ2.java
@@ -10,41 +10,40 @@ public class HostCountReducer extends Reducer<Text, Text, DoubleWritable, Double
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
-        long max = -1;
-        long min = -1;
-        long totalBytes = 0;
+        double max = -1;
+        double min = -1;
         int i = 0;
         String strKey = key.toString();
         double bytes = Double.parseDouble(strKey);
         double timeStamp = 0;
                 
         for (Text value : values) {
-        String val = value.toString();
-        timeStamp = Double.parseDouble(val);
-        }
+            String val = value.toString();
+            timeStamp = Double.parseDouble(val);
 
+        if (i == 0) {
+            max = timeStamp; //To start off, set the first value as the max
+            min = timeStamp; //Set the first value as the min as well
+            i++;
+        }
             
-           // if (i == 0) {
-             //   max = convertedVal; //To start off, set the first value as the max
-               // min = convertedVal; //Set the first value as the min as well
-                //i++;
-            //}
+        if (timeStamp > max) {
+            max = timeStamp;
+        }
+        
+        if (timeStamp < min) {
+            min = timeStamp;
+         }
             
-            //if (convertedVal > max) {
-              //  max = convertedVal;
-            //}
-            
-            //if (convertedVal < min) {
-             //   min = convertedVal;
-            //}
-            
-      //  }
+        }
                    
 //         for (Text key : keys) { //Is this iteration correct? No -- I think this can be done more easily in the command line?
 //             String stringKey = key.toString();
 //             long bytes = Long.parseLong(stringKey);
 //             totalBytes += bytes; //Sum all the bytes
 //         }
-        context.write(new DoubleWritable(bytes), new DoubleWritable(timeStamp));
+        double elapsedTime = max - min;
+        
+        context.write(new DoubleWritable(bytes), new DoubleWritable(elapsedTime));
     }
 }
