dc76a29355c102eddf83880021269dd47844ec7e
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2019-02-13 06:32:36 -0500

    Changed key from LongWritable to DoubleWritable

--- HostCountQ2.java
+++ HostCountQ2.java
@@ -27,7 +27,7 @@ public class HostCount {
         // set the format of the keys and values
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(Text.class);
-        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputKeyClass(DoubleWritable.class);
         job.setOutputValueClass(LongWritable.class);
         // submit the job and wait for its completion
         System.exit(job.waitForCompletion(true) ? 0 : 1);
