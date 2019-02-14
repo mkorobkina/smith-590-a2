4b427510d3fa16cfb2d656b5ca1af07491527ef6
Mila Korobkina <milenak@live.unc.edu>
2019-02-14 13:51:11 -0500

    Update HostCountMapperQ1.java

--- HostCountMapperQ1.java
+++ HostCountMapperQ1.java
@@ -1,7 +1,3 @@
-
-// map function for application to count the number of
-// times each unique IP address 4-tuple appears in an
-// adudump file.
 import java.io.IOException;
 import java.util.*;
 import java.io.*;
@@ -9,26 +5,36 @@ import java.net.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.util.*;
 import org.apache.hadoop.mapreduce.Mapper;
-
-public class HostCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    @Override
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        String[] tokens = line.split("\\s");
-        String IPaddr1 = new String();
-        String IPaddr2 = new String();
-        int last_dot;
-        // get the two IP address.port fields
-        IPaddr1 = tokens[2];
-        IPaddr2 = tokens[4];
-        // eliminate the port part
-        last_dot = IPaddr1.lastIndexOf('.');
-        IPaddr1 = IPaddr1.substring(0, last_dot);
-        last_dot = IPaddr2.lastIndexOf('.');
-        IPaddr2 = IPaddr2.substring(0, last_dot);
-        // output the key, value pairs where the key is an
-        // IP address 4-tuple and the value is 1 (count)
-        context.write(new Text(IPaddr1), new IntWritable(1));
-        context.write(new Text(IPaddr2), new IntWritable(1));
-    }
+public class HostCountMapper
+extends Mapper<LongWritable, Text, Text, Text> {
+@Override
+	public void map(LongWritable key, Text value, Context context)
+		throws IOException, InterruptedException {
+			String line = value.toString();
+			String[] tokens = line.split("\\s");
+			String IPaddr1 = new String();
+			String IPaddr2 = new String();
+			String direction = new String();
+			String byteValue = new String();
+			int last_dot;
+			//get the two IP address.port fields 
+			IPaddr1 = tokens[2];
+			IPaddr2 = tokens[4];
+			direction = tokens[3];
+			byteValue = tokens[5];
+			//get the port part (may want to change value names?)
+			last_dot = IPaddr1.lastIndexOf('.'); 
+			IPaddr1 = IPaddr1.substring(last_dot + 1, IPaddr1.length);//last part of first ip w port
+			last_dot = IPaddr2.lastIndexOf('.');
+			IPaddr2 = IPaddr2.substring(last_dot, last_dot); //last part of ip w port
+			//output the key, value pairs where the key is an
+			//IP address 4-tuple and the value is 1 (count) + byte value
+			if (direction.equals(">")) {
+				context.write(new Text(IPaddr1), new Text("1 " + byteValue));
+			}
+			else {
+				context.write(new Text(IPaddr2), new Text("1 " + byteValue));
+			}
+		}
 }
+
